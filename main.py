d41300060aecdf458ab734e70f33c6897f842396
ZeroDawn0D <umang26112003@gmail.com>
2020-08-17 16:02:09 +0530

    made pageSearch a function

--- main.py
+++ main.py
@@ -1,35 +1,63 @@
 # In search:
 # Movie list: id #titlelist
 # Song list: id #songlist
-# span class .homesongs a class .list-group-items
-#
+# a -> href(link) text(anime/song name)
+# TODO: fix song name 
 from bs4 import BeautifulSoup;
 import requests;
-def getOddIndex(i):
-	return 2*i+1;
-def removeBreaks(str): #Remove newlines and trailing spaces
+
+BASE_URL = "https://www.animesonglyrics.com/";
+SEARCH = BASE_URL+"results";
+
+#Remove newlines and trailing spaces
+def removeBreaks(str):
 	newstr = "";
 	for c in str:
 		if(c!="\n"):
 			newstr = newstr + c;
 	return newstr.strip();
 
+#pass 'a' tag object, get Name and Link
+def getNameLink(a):
+	return (removeBreaks(a.get_text()), a['href']);
 
-BASE_URL = "https://www.animesonglyrics.com/";
-RESULTS = "results";
-q = input("enter name: ");
+#uses the page's search function to generate a list of anime or songs
+#and the links to thier pages
+def pageSearch(query = "", listtype = "A"): 
+	idType = "";
+	if listtype == "A":
+		idType = "titlelist";
+	elif listtype == "S":
+		idType = "songlist";
+	else:
+		print("wrongChoice");
+		return;
+	param = {"q":query};
+	r = requests.get(SEARCH, params = param);
+	soup = BeautifulSoup(r.content, 'html.parser');
+	mainlist = soup.find(id = idType);
+	achild = mainlist.findChildren("a", recursive = "True");
+	global nameLink;
+	nameLink = [('','')] * len(achild);
+	c = 0;
+	for i in achild:
+		nameLink[c] = getNameLink(i);
+		#if listtype == "S":
+		#	nameLink[c] = (formatSongName(nameLink[c][0]),nameLink[c][1]);
+		c+=1;
+
+
+
+
+
+q = input("enter anime: ");
 q.strip();
 q = '+'.join(q.split());
-SEARCH = BASE_URL+RESULTS;
-query = {"q":q};
-print(SEARCH);
-r = requests.get(SEARCH,params = query)
-soup = BeautifulSoup(r.content, 'html.parser');
-titlelist = soup.find(id = "titlelist");
-achild = titlelist.findChildren("a", recursive = "True");
-def getNameLink(a): #pass 'a' tag object, get Name and Link
-	return (removeBreaks(a.get_text()), a['href']);
-for i in achild:
-	(a,b) = getNameLink(i);
-	print(a);
-	
+listtype = input("enter type: (A)nime/(S)ong: ").upper();
+listtype.strip();
+
+pageSearch(q,listtype);
+c = 0;
+for i in nameLink:
+	print(str(c+1) + ": " + i[0]);
+	c+=1;
