16cf99360c705508810f456c8b1364a2c2c5fe21
ZeroDawn0D <umang26112003@gmail.com>
2020-08-17 13:27:41 +0530

    search implemented

--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from bs4 import BeautifulSoup;
 import requests;
 def getOddIndex(i):
 	return 2*i+1;
-
+def removeBreaks(str): #Remove newlines and trailing spaces
+	newstr = "";
+	for c in str:
+		if(c!="\n"):
+			newstr = newstr + c;
+	return newstr.strip();
 
 
 BASE_URL = "https://www.animesonglyrics.com/";
@@ -20,14 +25,11 @@ query = {"q":q};
 print(SEARCH);
 r = requests.get(SEARCH,params = query)
 soup = BeautifulSoup(r.content, 'html.parser');
-while 1:
-	ans = int(input("enter index: "));
-	if (ans==-1):
-		break;
-	titlelist = soup.find(id = "titlelist").contents;
-	print(titlelist[getOddIndex(ans)]);
-	linktag = titlelist[getOddIndex(ans)].a;
-	link = linktag['href'];
-	print(link);
-	name = linktag.contents[2];
-	print(name);
+titlelist = soup.find(id = "titlelist");
+achild = titlelist.findChildren("a", recursive = "True");
+def getNameLink(a): #pass 'a' tag object, get Name and Link
+	return (removeBreaks(a.get_text()), a['href']);
+for i in achild:
+	(a,b) = getNameLink(i);
+	print(a);
+	
